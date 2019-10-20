2424fb9af45793b9ba701f0f8293bdb11160dfc8
Kyle <kylestang.ks@gmail.com>
2019-10-19 17:50:57 -0700

    Finished basic UI

--- app/src/main/java/ca/uvic/k4ncelled/MainActivity.java
+++ app/src/main/java/ca/uvic/k4ncelled/MainActivity.java
@@ -1,21 +1,15 @@
 package ca.uvic.k4ncelled;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.time.LocalDate;
 
@@ -23,9 +17,15 @@ import ca.uvic.k4ncelled.Backend.Food;
 import ca.uvic.k4ncelled.Backend.Fridge;
 
 public class MainActivity extends AppCompatActivity {
+    private Fridge myFridge;
+
     private Spinner sp_selectFood;
     private TextView tv_selectedInfo;
     private Button bt_addFood;
+    private Button bt_removeFood;
+    private EditText et_foodName;
+    private EditText et_purchaseDate;
+    private EditText et_expiryDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Fridge myFridge = new Fridge();
+        myFridge = new Fridge();
         myFridge.addFood("apple", LocalDate.now(), LocalDate.of(2021, 1, 1));
         myFridge.addFood("orange", LocalDate.now(), LocalDate.of(2022, 1, 1));
         myFridge.addFood("banana", LocalDate.now(), LocalDate.of(2020, 1, 1));
@@ -41,17 +41,24 @@ public class MainActivity extends AppCompatActivity {
         sp_selectFood = findViewById(R.id.sp_selectFood);
         tv_selectedInfo = findViewById(R.id.tv_selectedInfo);
         bt_addFood = findViewById(R.id.bt_addFood);
+        bt_removeFood = findViewById(R.id.bt_removeFood);
+        et_foodName = findViewById(R.id.et_foodName);
+        et_purchaseDate = findViewById(R.id.et_purchaseDate);
+        et_expiryDate = findViewById(R.id.et_expiryDate);
 
-        create_sp_selectFood(myFridge);
+        create_sp_selectFood();
         create_bt_addFood();
+        create_bt_removeFood();
     }
 
-    private void create_sp_selectFood(Fridge myFridge){
+    private void create_sp_selectFood(){
         ArrayAdapter<Food> arrayAdapter = new ArrayAdapter<>(
                 getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, myFridge.getStorage());
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         sp_selectFood.setAdapter(arrayAdapter);
+
+        if(arrayAdapter.getCount() == 0){ tv_selectedInfo.setText(R.string.fridge_empty); }
 
         sp_selectFood.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -73,7 +80,21 @@ public class MainActivity extends AppCompatActivity {
         bt_addFood.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                myFridge.addFood(et_foodName.getText().toString(), et_purchaseDate.getText().toString(), et_expiryDate.getText().toString());
+                et_foodName.setText("");
+                et_purchaseDate.setText("");
+                et_expiryDate.setText("");
+                create_sp_selectFood();
+            }
+        });
+    }
 
+    private void create_bt_removeFood(){
+        bt_removeFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myFridge.removeFood((Food) sp_selectFood.getSelectedItem());
+                create_sp_selectFood();
             }
         });
     }
