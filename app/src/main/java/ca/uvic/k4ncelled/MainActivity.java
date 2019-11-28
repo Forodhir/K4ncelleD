57c8dc34012c868ad5843053dceb219b45afbce2
Kyle <kylestang.ks@gmail.com>
2019-11-28 10:03:32 -0800

    UI

--- app/src/main/java/ca/uvic/k4ncelled/MainActivity.java
+++ app/src/main/java/ca/uvic/k4ncelled/MainActivity.java
@@ -1,6 +1,8 @@
 package ca.uvic.k4ncelled;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -9,7 +11,12 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import ca.uvic.k4ncelled.Backend.Food;
 import ca.uvic.k4ncelled.Backend.Fridge;
@@ -19,13 +26,12 @@ public class MainActivity extends AppCompatActivity {
     private Fridge fridge;
     private Data data;
 
-    private Spinner sp_selectFood;
-    private TextView tv_selectedInfo;
-    private Button bt_addFood;
-    private Button bt_removeFood;
-    private EditText et_foodName;
-    private EditText et_purchaseDate;
-    private EditText et_expiryDate;
+    private ConstraintLayout activeLayout;
+
+    private ConstraintLayout lo_browse;
+    private ConstraintLayout lo_add;
+
+    private BottomNavigationView nm_navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,66 +42,44 @@ public class MainActivity extends AppCompatActivity {
 
         fridge = data.load();
 
-        sp_selectFood = findViewById(R.id.sp_selectFood);
-        tv_selectedInfo = findViewById(R.id.tv_selectedInfo);
-        bt_addFood = findViewById(R.id.bt_addFood);
-        bt_removeFood = findViewById(R.id.bt_removeFood);
-        et_foodName = findViewById(R.id.et_foodName);
-        et_purchaseDate = findViewById(R.id.et_purchaseDate);
-        et_expiryDate = findViewById(R.id.et_expiryDate);
+        assignComponents();
 
-        create_sp_selectFood();
-        create_bt_addFood();
-        create_bt_removeFood();
+        setActiveLayout(lo_browse);
+
+        create_navigationView();
+
     }
 
-    private void create_sp_selectFood(){
-        ArrayAdapter<Food> arrayAdapter = new ArrayAdapter<>(
-                getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, fridge.getStorage());
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        sp_selectFood.setAdapter(arrayAdapter);
-
-        if(arrayAdapter.getCount() == 0){ tv_selectedInfo.setText(R.string.fridge_empty); }
-
-        sp_selectFood.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Food selected = (Food) sp_selectFood.getSelectedItem();
-                tv_selectedInfo.setText(selected.toString() + "\nPurchase date: "
-                        + selected.getPurchaseDate() + "\nExpiry date: "
-                        + selected.getExpiryDate());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
+    private void setActiveLayout(ConstraintLayout layout){
+        if(activeLayout != null){
+            activeLayout.setVisibility(View.GONE);
+        }
+        activeLayout = layout;
+        activeLayout.setVisibility(View.VISIBLE);
     }
 
-    private void create_bt_addFood(){
-        bt_addFood.setOnClickListener(new View.OnClickListener() {
+    private void assignComponents(){
+        nm_navigationView = findViewById(R.id.bottomNavigationView);
+        lo_browse = findViewById(R.id.lo_browse);
+        lo_add = findViewById(R.id.lo_add);
+    }
+
+    private void create_navigationView(){
+        nm_navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                fridge.addFood(et_foodName.getText().toString(), et_purchaseDate.getText().toString(), et_expiryDate.getText().toString());
-                et_foodName.setText("");
-                et_purchaseDate.setText("");
-                et_expiryDate.setText("");
-                create_sp_selectFood();
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId() == R.id.mi_browse){
+                    setActiveLayout(lo_browse);
+                }
+                else if(menuItem.getItemId() == R.id.mi_add){
+                    setActiveLayout(lo_add);
+                }
+                return true;
             }
         });
+        nm_navigationView.setSelectedItemId(R.id.mi_browse);
     }
 
-    private void create_bt_removeFood(){
-        bt_removeFood.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fridge.removeFood((Food) sp_selectFood.getSelectedItem());
-                create_sp_selectFood();
-            }
-        });
-    }
 
     @Override
     protected void onStop() {
